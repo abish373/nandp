58498dd0b748f58b512bfcde6ddafd6f29cc08bb
abish373 <abishp3@gmail.com>
2020-03-14 15:43:59 +0530

    working

--- python/hello.py
+++ python/hello.py
@@ -1,19 +1,18 @@
-
+import sys
 from gtts import gTTS 
  
 
-def something():
-	 mytext = 'Welcome to geeksforgeeks!'
+def speech():
+	 mytext = sys.argv[1]
   
 	 language = 'en'
   
 
 	 myobj = gTTS(text=mytext, lang=language, slow=False) 
 
-	 myobj.save("mpfile/welcome.mp3") 
+	 myobj.save("python/mpfile/" + sys.argv[1] + ".mp3") 
+
 	 
-	 return "Hello"
+speech()
 	 
-something()
-	 
-print("This is python text")
+print(sys.argv[1])
--- server.js
+++ server.js
@@ -1,42 +1,58 @@
 const express = require('express');
 const app = express()
 const bodyParser = require('body-parser')
+const path = require('path')
 
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+// app.use(express.static('python'));
+// app.use(express.static('views'));
+
+app.use('/python', express.static(path.join(__dirname, 'python')))
+app.use('/views', express.static(path.join(__dirname, 'views')))
 
 
 app.set('view engine','ejs')
 
+
+
+
 app.get('/',(req,res)=>{
-	res.render("index")
+	res.render('index');
 })
 
-app.post('/output',(req,res)=>{
-	console.log(req.body.usertext)
-	res.send(req.body.usertext)
+app.get('/output',(req,res)=>{
+	// console.log(req.body.usertext)
+	res.send("req.body.usertext")
 })
 
+// app.get('/test',(req,res)=>{
+// 	res.render("test")
+// })
 
 
-app.get('/name', callName); 
+app.post('/name', callName); 
   
 function callName(req, res) { 
+	const text = {name:req.body.usertext}
      
     const spawn = require("child_process").spawn; 
  
-    const processes = spawn('python',["python/hello.py"]);
+    const processes = spawn('python',["python/hello.py",req.body.usertext]);
  
     // console.log(process)
 
     processes.stdout.on('data', (data)=> { 
-        res.send(data.toString()); 
+        res.render('output',{text}); 
     } ) 
+
+    processes.stderr.on('data',(data)=>{
+    	console.log(data)
+    	res.send(data.toString())
+    })
 } 
-
-
 
 
 app.listen(80,()=>console.log("server started at port 80"))
